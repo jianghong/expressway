b448dcfe960272da17307ff0eb43991e2a15a6ff
Jackson Hong <jacksonxxhong@gmail.com>
2014-11-02 18:10:45 -0500

    feature/search: set up routes

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,22 @@
-angular.module('ExpressWay', ['ngMaterial'])
+var app = angular.module('ExpressWay', ['ngRoute', 'ngMaterial']);
 
-.controller('MainController', function($scope) {
+app.config(['$routeProvider',
+  function($routeProvider){
+    $routeProvider.
+      when('/intro', {
+        templateUrl: 'templates/intro.html',
+        controller: 'IntroController'
+      }).
+      when('/search', {
+        templateUrl: 'templates/search.html',
+        controller: 'SearchController'
+      }).
+      otherwise({
+        redirectTo: '/intro'
+      });
+  }])
+
+app.controller('MainController', function($scope, $location, $rootScope) {
   var MAIN_THEME_CLASS = 'md-blue-theme';
   var hamburgerIconSrc = 'static/img/hamburger-icon.png';
   var backIconSrc = 'static/img/back-icon.png';
@@ -13,20 +29,22 @@ angular.module('ExpressWay', ['ngMaterial'])
   $scope.searchIconSrc = searchIconWSrc;
 
   $scope.searchContext = function() {
-    switchContext('search');
+
+    $location.path('/search');
   }
 
-  $scope.mainContext = function() {
-    switchContext('main');
+  $scope.introContext = function() {
+    $location.path('/intro');
   } 
 
   function switchContext(context) {
     console.log(context);
-    if (context === 'search') {
+    if (context === '/search') {
       $scope.leftControlTitle = 'Back';
       $scope.isSearchContext = true;
       $scope.leftControlIconSrc = backIconSrc;      
       $scope.searchIconSrc = searchIconBSrc;
+
     } else {
       $scope.leftControlTitle = 'ExpressWay';
       $scope.isSearchContext = false;
@@ -34,4 +52,22 @@ angular.module('ExpressWay', ['ngMaterial'])
       $scope.searchIconSrc = searchIconWSrc;
     }
   }
+
+  $rootScope.switchContext = switchContext;
 })
+
+app.controller('IntroController', function($scope) {
+
+});
+
+app.controller('SearchController', function($scope) {
+});
+
+app.run( function($rootScope, $location) {
+   $rootScope.$watch(function() { 
+      return $location.path(); 
+    },
+    function(a){  
+      $rootScope.switchContext(a);
+    });
+});
