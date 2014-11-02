d15b481d2212c82e71717fe92c0f72335abd908d
Jackson Hong <jacksonxxhong@gmail.com>
2014-11-02 17:24:55 -0500

    feature/search:context switch css

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,37 @@
 angular.module('ExpressWay', ['ngMaterial'])
 
 .controller('MainController', function($scope) {
+  var MAIN_THEME_CLASS = 'md-blue-theme';
+  var hamburgerIconSrc = 'static/img/hamburger-icon.png';
+  var backIconSrc = 'static/img/back-icon.png';
+  var searchIconWSrc = 'static/img/search-icon.png';
+  var searchIconBSrc = 'static/img/search-icon-b.png';
 
+  $scope.isSearchContext = false;
+  $scope.leftControlIconSrc = hamburgerIconSrc;
+  $scope.leftControlTitle = 'ExpressWay';
+  $scope.searchIconSrc = searchIconWSrc;
+
+  $scope.searchContext = function() {
+    switchContext('search');
+  }
+
+  $scope.mainContext = function() {
+    switchContext('main');
+  } 
+
+  function switchContext(context) {
+    console.log(context);
+    if (context === 'search') {
+      $scope.leftControlTitle = 'Back';
+      $scope.isSearchContext = true;
+      $scope.leftControlIconSrc = backIconSrc;      
+      $scope.searchIconSrc = searchIconBSrc;
+    } else {
+      $scope.leftControlTitle = 'ExpressWay';
+      $scope.isSearchContext = false;
+      $scope.leftControlIconSrc = hamburgerIconSrc
+      $scope.searchIconSrc = searchIconWSrc;
+    }
+  }
 })
