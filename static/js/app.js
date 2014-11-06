ef5bf5ae27079295f4ccb51ef98da930c141b154
Jackson Hong <jacksonxxhong@gmail.com>
2014-11-06 12:09:16 -0500

    feature/beta: autocomplete and results positioning

--- static/js/app.js
+++ static/js/app.js
@@ -60,15 +60,10 @@ app.controller('MainController', function($scope, $location, $rootScope, answers
     },
     {
       question: 'What work programs can I apply for?'
-    },
-    {
-      question: 'What is the Federal Skilled Worker Program?'
     }
-
   ];
 
   $scope.searchContext = function() {
-    // $location.path('/search');
     $scope.switchContext('/search');
 
   }
